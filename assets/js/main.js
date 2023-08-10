5bcd190b0dcb8a9e0651935cd6b717debc998158
长高 <2320404851@qq.com>
2023-08-10 22:00:17 +0800

    Update scss.file and js.file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,36 @@
-
-$(document).ready(function() {
+$(document).ready(function () {
   // Create two variable with the names of the months and days in an array
-  var monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ]; 
-  var dayNames= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
-  
+  var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
   // Create a newDate() object
   var newDate = new Date();
   // Extract the current date from Date object
   newDate.setDate(newDate.getDate());
   // Output the day, date, month and year   
   $('#Date').html(dayNames[newDate.getDay()] + " " + newDate.getDate() + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear());
-  
-  setInterval( function() {
+
+  setInterval(function () {
     // Create a newDate() object and extract the seconds of the current time on the visitor's
     var seconds = new Date().getSeconds();
     // Add a leading zero to seconds value
-    $("#sec").html(( seconds < 10 ? "0" : "" ) + seconds);
-    },1000);
-    
-  setInterval( function() {
+    $("#sec").html((seconds < 10 ? "0" : "") + seconds);
+  }, 1000);
+
+  setInterval(function () {
     // Create a newDate() object and extract the minutes of the current time on the visitor's
     var minutes = new Date().getMinutes();
     // Add a leading zero to the minutes value
-    $("#min").html(( minutes < 10 ? "0" : "" ) + minutes);
-      },1000);
-    
-  setInterval( function() {
+    $("#min").html((minutes < 10 ? "0" : "") + minutes);
+  }, 1000);
+
+  setInterval(function () {
     // Create a newDate() object and extract the hours of the current time on the visitor's
     var hours = new Date().getHours();
     // Add a leading zero to the hours value
-    $("#hours").html(( hours < 10 ? "0" : "" ) + hours);
-      }, 1000);	
-  });
+    $("#hours").html((hours < 10 ? "0" : "") + hours);
+  }, 1000);
+});
 
 var a_idx = 0;
 jQuery(document).ready(function ($) {
@@ -91,12 +90,11 @@ jQuery(document).ready(function ($) {
   });
 });
 
-$(document).ready(function(){
-  for(let i=0;i<slides.length;i++){
-    $(".mySlides > img")[i].src="../../assets/img/img"+i+".jpg";
+$(document).ready(function () {
+  for (let i = 0; i < slides.length; i++) {
+    $(".mySlides > img")[i].src = "../../assets/img/img" + i + ".jpg";
   }
 }
-
 )
 let slideIndex = 0;
 let slides = $(".mySlides");
@@ -112,7 +110,7 @@ function plusSlides(n) {
 // Thumbnail image controls
 function currentSlide(n) {
   slidePreviousIndex = slideIndex;
-  slideIndex=n-1;
+  slideIndex = n - 1;
   showSlides(slidePreviousIndex);
 }
 
